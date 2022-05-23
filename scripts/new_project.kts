dbfc3ced789d52f337e9aca5149edf8169e29c6f
Wadeewee <suphawadee@nimblehq.co>
2022-05-23 09:38:30 +0700

    [#199] Show error message for missing/invalid arguments

--- scripts/new_project.kts
+++ scripts/new_project.kts
@@ -9,23 +9,67 @@ object NewProject {
     private const val TEMPLATE_FOLDER_NAME = "CoroutineTemplate"
     private const val TEMPLATE_PACKAGE_NAME = "co.nimblehq.coroutine"
 
+    private const val APP_PATTERN = "^[A-Z\$][a-zA-Z0-9\$]*\$"
+    private const val PACKAGE_PATTERN = "^[a-z]+(\\.[a-z][a-z0-9]*)*\$"
+
     private val modules = listOf("app", "data", "domain")
 
     private var appName = ""
-    private val appNameWithoutSpace: String
-        get() = appName.replace(" ", "")
     private var packageName = ""
+
     private val projectPath: String
-        get() = rootPath + appNameWithoutSpace
+        get() = rootPath + appName
+
     private val rootPath: String
         get() = System.getProperty("user.dir").replace("scripts", "")
 
     fun generate(args: Array<String>) {
         handleArguments(args)
-        initializeNewProjectFolder()
-        cleanNewProjectFolder()
-        renamePackageNameFolders()
-        renamePackageNameWithinFiles()
+    }
+
+    private fun handleArguments(args: Array<String>) {
+        val agrumentError = "ERROR: Invalid Agrument name: Ensure define argruments => app-name={\"MyProject\"} package-name={com.sample.myproject}"
+        when (args.size) {
+            1 -> when {
+                args.first().startsWith(KEY_APP_NAME) -> showMessage("ERROR: No package has been provided")
+                args.first().startsWith(KEY_PACKAGE_NAME) -> showMessage("ERROR: No app name has been provided")
+                else -> showMessage(agrumentError)
+            }
+            2 -> args.map { arg ->
+                val (key, value) = arg.split(ARGUMENT_DELIMITER)
+                when (key) {
+                    KEY_APP_NAME -> validAppName(value)
+                    KEY_PACKAGE_NAME -> validPackageName(value)
+                    else -> showMessage(agrumentError)
+                }.also { executeNextSteps() }
+            }
+            else -> showMessage("ERROR: Require app-name and package-name to initialize the new project")
+        }
+    }
+
+    private fun validAppName(value: String) {
+        if (APP_PATTERN.toRegex().containsMatchIn(value)) {
+            appName = value
+        } else {
+            showMessage("ERROR: Invalid App Name: $value (needs to follow standard pattern {AppName})")
+        }
+    }
+
+    private fun validPackageName(value: String) {
+        if (PACKAGE_PATTERN.toRegex().containsMatchIn(value)) {
+            packageName = value
+        } else {
+            showMessage("ERROR: Invalid Package Name: $value (needs to follow standard pattern {com.example.package})")
+        }
+    }
+
+    private fun executeNextSteps() {
+        if (appName.isNotEmpty() && packageName.isNotEmpty()) {
+            initializeNewProjectFolder()
+            cleanNewProjectFolder()
+            renamePackageNameFolders()
+            renamePackageNameWithinFiles()
+        }
     }
 
     private fun initializeNewProjectFolder() {
@@ -33,16 +77,6 @@ object NewProject {
         copyFiles(fromPath = rootPath + TEMPLATE_FOLDER_NAME, toPath = projectPath)
         // Set gradlew file as executable, because copying files from one folder to another doesn't copy file permissions correctly (= read, write & execute).
         File(projectPath + File.separator + "gradlew")?.setExecutable(true)
-    }
-
-    private fun handleArguments(args: Array<String>) {
-        args.forEach {
-            val (key, value) = it.split(ARGUMENT_DELIMITER)
-            when (key) {
-                KEY_APP_NAME -> appName = value
-                KEY_PACKAGE_NAME -> packageName = value
-            }
-        }
     }
 
     private fun renamePackageNameFolders() {
@@ -75,10 +109,6 @@ object NewProject {
                     tempDirectory.deleteRecursively()
                 }
         }
-    }
-
-    private fun showMessage(message: String) {
-        println("\n${message}\n")
     }
 
     private fun copyFiles(fromPath: String, toPath: String) {
@@ -124,6 +154,10 @@ object NewProject {
         var sourceText = sourceFile.readText()
         sourceText = sourceText.replace(oldValue, newValue)
         sourceFile.writeText(sourceText)
+    }
+
+    private fun showMessage(message: String) {
+        println("\n${message}\n")
     }
 }
 
